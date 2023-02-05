0c2342803d263b74175c810407a4aca48d050a4c
Foxtrot47 <jjneutron@outlook.com>
2023-02-05 13:14:46 +0530

    Initial Scraper Logic

--- Torrent Scaper/Scraper.cs
+++ Torrent Scaper/Scraper.cs
@@ -14,12 +14,12 @@ namespace TorrentScaperUtil
             PageCount=0;
         }
 
-        public Scraper GetResults(string[] arr)
+        public Scraper GetResults(string[] arr,int pageNumber)
         {
-            Data = Data.Concat(Scrape1337xPages(arr).Result).ToList();
+            Data = Data.Concat(Scrape1337XPages(arr,pageNumber).Result).ToList();
             return this;
         }
-        public async Task<List<ScrapeData>> Scrape1337xPages (string[] keywords, int pageNumber = 1)
+        public async Task<List<ScrapeData>> Scrape1337XPages (string[] keywords, int pageNumber = 1)
         {
             const string domain = "https://1337x.to";
             var searchKeywords = keywords.Aggregate(
@@ -36,7 +36,7 @@ namespace TorrentScaperUtil
             PageCount = document.QuerySelectorAll("div.pagination > ul > li").Length;
             var data = new List<ScrapeData>(links.Count);
 
-            Parallel.ForEach(links, row => data.Add(Scrape1337xData(row!).Result));
+            Parallel.ForEach(links, row => data.Add(Scrape1337xData(row!)?.Result));
 
             return data;
         }
@@ -47,24 +47,27 @@ namespace TorrentScaperUtil
             var config = Configuration.Default.WithDefaultLoader();
             var context = BrowsingContext.New(config);
             var document = await context.OpenAsync(domain + link);
+            if (string.IsNullOrEmpty(document.QuerySelector("div.box-info-heading.clearfix")?.TextContent))
+                return null;
             return new ScrapeData()
             {
-                Title = document.QuerySelector("div.box-info-heading.clearfix")?.TextContent,
-                TorrentName = document.QuerySelector("div.box-info-heading.clearfix")?.TextContent,
-                Category = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(1) > span")?.TextContent,
-                Type = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(2) > span")?.TextContent,
-                Language = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(3) > span")?.TextContent,
-                TotalSize = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(4) > span")?.TextContent,
-                Downloads = document.QuerySelector("div.clearfix > ul:nth-child(3) > li:nth-child(1) > span")?.TextContent,
-                DateUploaded = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(3) > span")?.TextContent,
-                SeedersCount = int.TryParse(document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(4) > span")?.TextContent, out int seeders) ? seeders : default(int),
-                LeechersCount = int.TryParse(document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(5) > span")?.TextContent, out int leechers) ? leechers : default(int),
-                MagnetLink = document.QuerySelector("ul.l7b7a00afd5bb7c8bd0b02594adad5e2df6a0ed61 > li:nth-child(1) > a")?.TextContent,
+                Title = document.QuerySelector("div.box-info-heading.clearfix")?.TextContent.Trim(),
+                TorrentName = document.QuerySelector("div.box-info-heading.clearfix")?.TextContent.Trim(),
+                Category = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(1) > span")?.TextContent.Trim(),
+                Type = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(2) > span")?.TextContent.Trim(),
+                Language = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(3) > span")?.TextContent.Trim(),
+                TotalSize = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(4) > span")?.TextContent.Trim(),
+                Downloads = document.QuerySelector("div.clearfix > ul:nth-child(3) > li:nth-child(1) > span")?.TextContent.Trim(),
+                DateUploaded = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(3) > span")?.TextContent.Trim(),
+                SeedersCount = int.TryParse(document.QuerySelector("span.seeds")?.TextContent.Trim(), out int seeders) ? seeders : default(int),
+                LeechersCount = int.TryParse(document.QuerySelector("span.leeches")?.TextContent.Trim(), out int leechers) ? leechers : default(int),
+                MagnetLink = document.QuerySelector("div.clearfix > ul> li:nth-child(1) > a")?.GetAttribute("href"),
                 TorrentLink = document.QuerySelector("li.dropdown > ul > li:nth-child(1) > a")?.GetAttribute("href"),
-                Description = document.QuerySelector("#mCSB_1_container > p")?.TextContent,
+                Description = document.QuerySelector("#mCSB_1_container > p")?.TextContent.Trim(),
                 WebSiteName = "1337x",
                 ImageUrl = document.QuerySelector("div.torrent-image-wrap > div > img")?.GetAttribute("src"),
                 OriginUrl = domain + link,
+                Uploader = document.QuerySelector("div.clearfix > ul:nth-child(2) > li:nth-child(5) > span > a")?.TextContent.Trim()
             };
         }
     }
