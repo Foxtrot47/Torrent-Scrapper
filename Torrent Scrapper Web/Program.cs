63b50457833925667e5468f03a4a373bb022a88d
Foxtrot47 <jjneutron@outlook.com>
2023-02-05 13:06:56 +0530

    Enable Razor Runtime Page Compilation

--- Torrent Scrapper Web/Program.cs
+++ Torrent Scrapper Web/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
